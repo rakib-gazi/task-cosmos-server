604bd8ba1a84f419187525c0bdbd774ee53aa4b3
Rakib Gazi <gazicourses@gmail.com>
2025-02-21 12:11:19 +0600

    tasks get api is added

--- index.js
+++ index.js
@@ -1,21 +1,36 @@
 const express = require("express");
 const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
 require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.whnyl.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
 async function run() {
-    try {
-  
-      // Root Api
+  try {
+    const taskCollection = client
+      .db("Task Cosmos")
+      .collection("tasks");
+    // Root Api
     app.get("/", (req, res) => {
-        res.send("DnD  server is running");
-      });
-     
-    } finally {
-    }
+      res.send("DnD  server is running");
+    });
+    // tasks get api for getting all task from database
+    app.get("/tasks", async (req, res) => {
+        const tasks = await taskCollection.find().toArray();
+        res.json(tasks);
+    });
+  } finally {
   }
-  run();
-  
-  app.listen(port, () => {});
+}
+run();
+
+app.listen(port, () => {});
