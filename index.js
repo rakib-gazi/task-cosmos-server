cc54adab8ebdcb61043fe4409d8914520a938ea0
Rakib Gazi <gazicourses@gmail.com>
2025-02-21 14:35:39 +0600

    socket.io added in backedend for realtime update

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
 const express = require("express");
 const cors = require("cors");
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const { createServer } = require("http");
+const { Server } = require("socket.io");
 require("dotenv").config();
+
 const app = express();
+const httpServer = createServer(app);
+const io = new Server(httpServer, {
+  cors: {
+    origin: "*",
+  },
+});
 const port = process.env.PORT || 5000;
+
 app.use(cors());
 app.use(express.json());
+
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.whnyl.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 const client = new MongoClient(uri, {
   serverApi: {
@@ -14,12 +25,21 @@ const client = new MongoClient(uri, {
     deprecationErrors: true,
   },
 });
+
 async function run() {
   try {
     const taskCollection = client.db("Task Cosmos").collection("tasks");
     // Root Api
     app.get("/", (req, res) => {
       res.send("DnD  server is running");
+    });
+    // socket.io connection
+    io.on("connection", (socket) => {
+      console.log("A user connected:", socket.id);
+
+      socket.on("disconnect", () => {
+        console.log("A user disconnected:", socket.id);
+      });
     });
     // tasks get api for getting all task from database
     app.get("/tasks", async (req, res) => {
@@ -44,6 +64,8 @@ async function run() {
       };
 
       const result = await taskCollection.insertOne(newTask);
+    //   emit for realtime update
+      io.emit("task_updated", { action: "add", task: newTask });
       res.json(result);
     });
     // task put api for update tasks
@@ -60,12 +82,16 @@ async function run() {
         },
       };
       const result = await taskCollection.updateOne(query, updatedTask);
+    //   emit for realtime update
+    io.emit("task_updated", { action: "update", taskId: id, updatedTask });
       res.json(result);
     });
     // task delete api for removed tasks
     app.delete("/tasks/:id", async (req, res) => {
       const { id } = req.params;
       const result = await taskCollection.deleteOne({ _id: new ObjectId(id) });
+    //   emit for realtime update
+    io.emit("task_updated", { action: "delete", taskId: id });
       res.json(result);
     });
   } finally {
